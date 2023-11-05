2b5986928ea5482740c2e4c651f885c5986e3cd9
Doniyor <ddonierov96@gmail.com>
2023-11-05 16:46:21 +0500

    fix bag

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -14,7 +14,7 @@ const AuthLayout = () => {
             <Outlet />
           </section>
           <img
-            src='./assets/images/side-img.svg'
+            src='/assets/images/side-img.svg'
             alt='logo'
             className='hidden xl:block h-screen w-1/2 object-cover bg-no-repeat'
           />
